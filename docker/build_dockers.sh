b86a65700e146e3448ca87e6858c347cf65e7ae9
Keith Turner <khturner@gmail.com>
2020-10-06 15:03:17 -0500

    Docker building complete + script

--- docker/build_dockers.sh
+++ docker/build_dockers.sh
@@ -5,14 +5,10 @@ IMAGE_PFX=bugpipe
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URL
 
-# Shovill
-IMAGE_NAME=shovill
-docker build --no-cache -t $ECR_URL/$IMAGE_PFX/$IMAGE_NAME -f $IMAGE_NAME/Dockerfile.$IMAGE_NAME $IMAGE_NAME
-aws ecr create-repository --repository-name $IMAGE_PFX/$IMAGE_NAME
-docker push $ECR_URL/$IMAGE_PFX/$IMAGE_NAME
-
-# Prokka
-IMAGE_NAME=prokka
-docker build --no-cache -t $ECR_URL/$IMAGE_PFX/$IMAGE_NAME -f $IMAGE_NAME/Dockerfile.$IMAGE_NAME $IMAGE_NAME
-aws ecr create-repository --repository-name $IMAGE_PFX/$IMAGE_NAME
-docker push $ECR_URL/$IMAGE_PFX/$IMAGE_NAME
+for IMAGE_NAME in shovill prokka quast sratoolkit trim_galore       
+do
+    docker build -t $IMAGE_PFX/$IMAGE_NAME -f $IMAGE_NAME/Dockerfile.$IMAGE_NAME $IMAGE_NAME
+    docker tag $IMAGE_NAME/$IMAGE_NAME:latest $ECR_URL/$IMAGE_PFX/$IMAGE_NAME:latest
+    aws ecr create-repository --repository-name $IMAGE_PFX/$IMAGE_NAME
+    docker push $ECR_URL/$IMAGE_PFX/$IMAGE_NAME
+done
