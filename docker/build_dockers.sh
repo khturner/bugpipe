4910d1f759982a4094ac6edbdf31975083498301
Keith Turner <khturner@gmail.com>
2020-10-06 15:22:47 -0500

    doh

--- docker/build_dockers.sh
+++ docker/build_dockers.sh
@@ -8,7 +8,7 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 for IMAGE_NAME in shovill prokka quast sratoolkit trim_galore       
 do
     docker build -t $IMAGE_PFX/$IMAGE_NAME -f $IMAGE_NAME/Dockerfile.$IMAGE_NAME $IMAGE_NAME
-    docker tag $IMAGE_NAME/$IMAGE_NAME:latest $ECR_URL/$IMAGE_PFX/$IMAGE_NAME:latest
+    docker tag $IMAGE_PFX/$IMAGE_NAME:latest $ECR_URL/$IMAGE_PFX/$IMAGE_NAME:latest
     aws ecr create-repository --repository-name $IMAGE_PFX/$IMAGE_NAME
     docker push $ECR_URL/$IMAGE_PFX/$IMAGE_NAME
 done
